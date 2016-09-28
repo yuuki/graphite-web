451875034c29cd530fa72bd92ed525e518270232
y_uuki <yuki.tsubo@gmail.com>
2016-09-28 13:11:17 +0900

    コメント

--- webapp/graphite/dynamodb.py
+++ webapp/graphite/dynamodb.py
@@ -6,13 +6,14 @@ from graphite.util import epoch
 
 class DynamoDB:
   def __init__(self):
+    # configファイルから各パラメータを指定できるようにしたい
     self.client = boto3.resource('dynamodb', region_name='ap-northeast-1', endpoint_url="http://localhost:8000")
 
   def fetch(self, name, startTime, endTime):
-    table = self.client.Table("metrictest01")
+    table = self.client.Table("metrictest01") # configファイルから指定できるようにしたい
     response = table.query(
         Select='ALL_ATTRIBUTES',
-        Limit=500,
+        Limit=500, # 適当
         ConsistentRead=False,
         KeyConditionExpression=Key('Name').eq(name) & Key("Timestamp").between(startTime, endTime),
     )
